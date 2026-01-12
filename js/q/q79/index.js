9170c7150e5f88ae3060870cdf6b80d08627a44c
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-01-12 16:06:39 +0900

    9일차 (2026-01-08-Thu) 실습
    js/q/q78~79
    java/HelloJava

---
+++ js/q/q79/index.js
@@ -0,0 +1,30 @@
+var str_id = "";
+var str_pw = "";
+
+var input_id;
+var input_pw;
+
+window.onload = function () {
+  // html 문서가 다 읽어진 후에 실행되는 함수임. 이걸 써야 아래 h1 태그(id=a)에 대한 접근이 가능해짐
+
+  input_id = document.getElementById("id");
+  input_pw = document.getElementById("pw");
+
+  //todo
+
+  // str_id = input_id.value;
+  // str_pw = input_pw.value;
+};
+
+function login() {
+  // alert("id:"+str_id+" pw:"+str_pw);
+  // alert("id:" + input_id.value + " pw:" + input_pw.value);
+  var local_id = "cat";
+  var local_pw = "1234";
+
+  if (local_id == input_id.value && local_pw == input_pw.value) {
+    login_box.innerHTML = local_id + " 회원님 반갑습니다.";
+  } else {
+    alert("아이디 또는 비밀번호를 다시 입력해주세요!");
+  }
+}
---
+++ js/q/q79/reg_member.js
@@ -0,0 +1,9 @@
+if (id.length >= 4 || id.length <= 12) {
+  alert("as");
+} else {
+  alert("id는 4글자이상 12자 이하로만 입력하세요");
+}
+
+if (pw.length != pw_check.length) {
+  alert("비밀번호가 일치하지 않습니다.");
+}
