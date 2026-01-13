c30989e4cd53180b920e0505c5a4c7542296a494
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-01-14 01:42:53 +0900

    12일차 (2026-01-13) 실습
    가위바위보 수정
    rpc02 미완

--- java/CatIsNewcat/src/com/watereagle/rpc/rpc01.java
+++ java/CatIsNewcat/src/com/watereagle/rpc/rpc01.java
@@ -6,26 +6,60 @@ public class rpc01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("[가위, 바위, 보 중 하나를 ");
+		System.out.println("[가위, 바위, 보 중 하나를 입력하세요]");
 		Scanner sc = new Scanner(System.in);
 		String cmd = sc.next();
 
-		int r = (int) (Math.random() * 2 + 1); // 1~3 까지 랜덤하게 뽑음
-
+		int r = (int) ((Math.random() * 2) + 1); // 1~3 까지 랜덤하게 뽑음
+		// 1가위 2바위 3보
 		String com = "오류";
-		switch (r) {
-		case 1: // 가위
-			com = "가위";
+		switch (cmd) {
+		case "가위": // 가위
+			switch (r) {
+			case 1:
+				System.out.println("비겼어요");
+				break;
+			case 2:
+				System.out.println("졌어요");
+				break;
+			case 3:
+				System.out.println("이겼어요");
+			default:
+				break;
+			}
 			break;
-		case 2:
-			com = "바위";
-		case 3:
-			com = "보";
+		case "바위":
+			switch (r) {
+			case 1:
+				System.out.println("이겼어요");
+				break;
+			case 2:
+				System.out.println("비겼어요");
+				break;
+			case 3:
+				System.out.println("졌어요");
+			default:
+				break;
+			}
+		case "보":
+			switch (r) {
+			case 1:
+				System.out.println("졌어요");
+				break;
+			case 2:
+				System.out.println("이겼어요");
+				break;
+			case 3:
+				System.out.println("비겼어요");
+				break;
+			default:
+				break;
+			}
+		case "x":
+			break;
 		default:
 			System.out.println("랜덤 값이 전송되지 않았습니다.");
 			break;
-			
-		// 이제 r하고 com 비교해서 가위바위보 승패 비긴 결과 코드 작성하기
 		}
 	}
 
---
+++ java/CatIsNewcat/src/com/watereagle/rpc/rpc02main.java
@@ -0,0 +1,11 @@
+package com.watereagle.rpc;
+
+import java.util.Scanner;
+
+public class rpc02main {
+
+	public static void main(String[] args) {
+		rpc02 a = new rpc02();
+		a.run();
+	}
+}
