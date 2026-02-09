4a258746e32358dac0f992e131892126f0577f9b
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-02-09 17:46:37 +0900

    애노테이션 기반 설정 ServletHelloWorld 추가, Test 수정

--- java/JavaServlet/src/main/java/com/watereagle/servlet/ServletHelloWorld.java
+++ java/JavaServlet/src/main/java/com/watereagle/servlet/ServletHelloWorld.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/ServletHelloWorld")
 public class ServletHelloWorld extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
--- java/JavaServlet/src/main/java/com/watereagle/servlet/Test.java
+++ java/JavaServlet/src/main/java/com/watereagle/servlet/Test.java
@@ -11,15 +11,14 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Test
  */
-@WebServlet("Test")
+@WebServlet("/Test")
 public class Test extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		System.out.println("==== 두 개");
-//		System.out.println("아이디 " + request.getParameter("id"));
-//		System.out.println("비밀번호: " + request.getParameter("pw"));
 		
 		response.sendRedirect("http://www.google.com/");
 	}
