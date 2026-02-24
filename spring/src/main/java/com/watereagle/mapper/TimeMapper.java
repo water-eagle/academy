455d6f6f3bd0ea9b47e9cbb19ede4aa832451d42
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-02-24 19:21:37 +0900

    [Spring][1+2]준비 {db 설정, test} Mapper 인터페이스 추가
    TimeMapper 인터페이스 추가, TimeMapperTests로 Mapper 유닛 테스트하기

---
+++ spring/src/main/java/com/watereagle/mapper/TimeMapper.java
@@ -0,0 +1,9 @@
+package com.watereagle.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+	//@Select("SELECT sysdate From dual")
+	@Select("SELECT now() From dual")
+	public String getTime();
+}
