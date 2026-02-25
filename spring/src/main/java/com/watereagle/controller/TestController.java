d32d34aee7a080beb49d5342e9d5149b41dac111
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-02-25 17:52:45 +0900

    [Spring][1+2]개발 - 3/4. 프레젠테이션(웹) 계층의 crud 구현
    1. 작업 폴더 src/main/java 에 com.watereagle.controller 패키지 추가
    2. TestController 클래스 추가
    com.watereagle.controller 패키지를 스프링에 관리하게 설정, 로깅하게 설정

---
+++ spring/src/main/java/com/watereagle/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.watereagle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.watereagle.service.TestService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RequestMapping("/test/*")
+@AllArgsConstructor
+@Controller
+public class TestController {
+	private TestService service;
+	
+	@GetMapping("/getOnePlusTwo")
+//	public void getOnePlusTwo(Model model) {
+	public void getOnePlusTwo() {
+		log.info("getOnePlusTwo ==========================================");
+		String one = service.getOne();
+		String two = service.getTwo();
+		Integer sum = Integer.parseInt(one) + Integer.parseInt(two); 
+		
+		log.info("(여기 컨트롤러임) 1 더하기 2는 = " + sum);
+		
+		//model.addAttribute("sum",sum);
+	}
+}
