e942e458e05afc0dae828f04c27f9c5406722d51
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-02-25 15:46:42 +0900

    [Spring][1+2]개발 - 2/4. 비지니스 계층의 crud 구현 - 3. 클래스 추가
    pom.xml에서 log4 dependency 태그 중 scope runtime 태그를 주석처리함

---
+++ spring/src/main/java/com/watereagle/service/TestServiceImpl.java
@@ -0,0 +1,38 @@
+package com.watereagle.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.watereagle.mapper.TestMapper;
+import com.watereagle.spring.dto.TestDto;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+//@AllArgsConstructor
+public class TestServiceImpl implements TestService{
+
+	@Setter(onMethod_ = @Autowired)
+	private TestMapper mapper;	
+	
+	@Override
+	public String getOne() {
+		log.info("test===========");
+		TestDto tvo = mapper.getData1();
+		String one = tvo.getStr_data();
+		return one;
+	}
+
+	@Override
+	public String getTwo() {
+		log.info("test===========");
+		TestDto tvo = mapper.getData2();
+		String two = tvo.getStr_data();
+		return two;
+	}
+	
+	
+
+}
