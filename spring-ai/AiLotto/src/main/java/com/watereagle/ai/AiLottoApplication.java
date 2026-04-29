b95f9c05f22a28253311a248111c969219b6dd73
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-04-29 19:53:41 +0900

    STS4 실습 AiLotto 프로젝트 생성

---
+++ spring-ai/AiLotto/src/main/java/com/watereagle/ai/AiLottoApplication.java
@@ -0,0 +1,13 @@
+package com.watereagle.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLottoApplication.class, args);
+	}
+
+}
---
+++ spring-ai/AiLotto/src/test/java/com/watereagle/ai/spring/AiLottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watereagle.ai.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
