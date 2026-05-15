ec9adcd2c3c515a93bf71b9b67fe3715620c425a
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-05-15 17:05:14 +0900

    AiBoard 프로젝트 생성

---
+++ spring-ai/aiboard/src/main/java/com/watereagle/academy/springai/aiboard/AiboardApplication.java
@@ -0,0 +1,13 @@
+package com.watereagle.academy.springai.aiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiboardApplication.class, args);
+	}
+
+}
---
+++ spring-ai/aiboard/src/test/java/com/watereagle/academy/springai/aiboard/AiboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watereagle.academy.springai.aiboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
